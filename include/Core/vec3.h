ef2ac9342683365d623c300460012c976ae98b86
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-03-05 12:19:19 +0800

    docs: add some comments bc yes

--- include/Core/vec3.h
+++ include/Core/vec3.h
@@ -5,25 +5,29 @@
 using namespace std;
 
 class Vec3 {
-    float x,y,z;
-    
     public:
+
+    float x,y,z;
     Vec3(float x, float y, float z);
+
+    // Basic operations
     Vec3 operator-() const;
     Vec3 operator+(const Vec3 &other) const;
     Vec3 operator-(const Vec3 &other) const;
     Vec3 operator*(float scalar) const;
     Vec3 operator/(float scalar) const;
 
+    // Some syntax sugar operations
     Vec3& operator+=(const Vec3 &other);
     Vec3& operator-=(const Vec3 &other);
     Vec3& operator*=(float scalar);
     Vec3& operator/=(float scalar);
-    
+
+    // Equality
     bool operator==(const Vec3 &other) const;
     bool operator!=(const Vec3 &other) const;
 
-
+    // ostream
     ostream& operator<<(ostream& os) const;
 
 };
