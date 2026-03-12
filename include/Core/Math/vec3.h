22e4502dc1070fe3d2e0b1f50b4e0c9ac5af16ac
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-03-12 15:44:47 +0800

    fix: missing default constructor for Vec3

--- include/Core/Math/vec3.h
+++ include/Core/Math/vec3.h
@@ -9,6 +9,7 @@ struct Vec3 {
     public:
 
     float x,y,z;
+    Vec3();
     Vec3(float x, float y, float z);
 
     // Operator overrides:
--- src/Core/Math/vec3.cpp
+++ src/Core/Math/vec3.cpp
@@ -1,5 +1,11 @@
 #include <vec3.h>
 
+Vec3::Vec3() {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
+}
+
 Vec3::Vec3(float x, float y, float z) {
     this->x = x;
     this->y = y;
